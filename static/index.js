0eff9aaf44d49c5ac490855f240f0038870548a1
Prashant Rastogi <publicidforpeople>
2025-01-17 17:32:13 +0530

    add curly braces open and close logic

--- static/index.js
+++ static/index.js
@@ -1,19 +1,43 @@
 // --------------------------------------------
+// selecting-elements by id and class
 const functionScopeConst =
   document.getElementsByClassName(`function-scope-const`);
 const operand = document.getElementById(`operand`);
 const functionName = document.getElementById(`functionName`);
 const functionMethod = document.getElementById(`functionMethod`);
 // --------------------------------------------
+const dollarCurlyBracesOpen = document.getElementsByClassName(
+  `dollar-curly-braces-open`
+);
+const dollarCurlyBracesClose = document.getElementsByClassName(
+  `dollar-curly-braces-close`
+);
+// --------------------------------------------
+// const
 const functionScopeConstLength = functionScopeConst.length;
 for (let index = 0; index < functionScopeConstLength; index++) {
   const element = functionScopeConst[index];
   element.innerText = `const`;
 }
+// --------------------------------------------
+// curlyBraces
+const dollarCurlyBracesOpenLength = dollarCurlyBracesOpen.length;
+const dollarCurlyBracesCloseLength = dollarCurlyBracesClose.length;
+for (let index = 0; index < dollarCurlyBracesOpenLength; index++) {
+  const element = dollarCurlyBracesOpen[index];
+  element.innerText = "${";
+}
+for (let index = 0; index < dollarCurlyBracesCloseLength; index++) {
+  const element = dollarCurlyBracesClose[index];
+  element.innerText = "}";
+}
+// --------------------------------------------
+// innerText
 operand.innerText = `=`;
 functionName.innerText = `document`;
 functionMethod.innerText = `.write`;
 // --------------------------------------------
+// copyBtn
 const copiedBtn = document.createElement(`span`);
 copiedBtn.className = `copy-text-done`;
 copiedBtn.innerText = `copied`;
